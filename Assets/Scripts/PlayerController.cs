7c9a3b1cf0c95f02487a2fe2a9f82caf88a1e555
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-07-14 14:54:24 -0400

    adding flatkit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// script controlls the player on their bike
+    /// 
+
+    [SerializeField] Transform frontWheel, backWheel; // our front and back wheels
+    [SerializeField] float moveSpeed;
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+}
