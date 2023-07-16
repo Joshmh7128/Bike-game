fa2467984db666caf4378ad6e436a3dd3924fe12
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-07-16 11:22:45 -0400

    working on the bike

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     /// script controlls the player on their bike
     /// 
 
-    [SerializeField] Transform frontWheel, backWheel; // our front and back wheels
-    [SerializeField] float moveSpeed;
+    [SerializeField] Rigidbody pedals; // our front and back wheels
+    [SerializeField] Rigidbody backWheel;
+    [SerializeField] float pedalTorqueMultiplier, backWheelTorqueMultiplier;
+    [SerializeField] InputAction pedalAction; // our input from our pedals
 
-    private void FixedUpdate()
+    public float forceToApply, readFloat; // the rotational force we want to apply
+    [SerializeField] float lastRight, lastLeft;
+    [SerializeField] bool onRight; // are we on our right or left foot?
+
+    // for our IK purposes
+    [SerializeField] Transform rightFootTarget, leftFootTarget, rightFootPop, leftFootPop;
+
+    private void OnEnable()
     {
-        
+        pedalAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pedalAction.Disable();
     }
 
 
+    private void FixedUpdate()
+    {
+        // first get our input
+        ReceiveInput();
+        // update feet
+        UpdateFeet();
+
+    }
+
+    void ReceiveInput()
+    {
+        // we only want to get the difference of input from each leg since the last input, until it passes our bottom out threshhold
+        // this is how a bike works: force is applied based on how much we push with each foot, until pushing with that foot does nothing else,
+        // and we have to switch to our other foot. we have to physically base the foot's threshold reset too, so that we force the player to use the other foot
+        /// forces are read like this!
+        readFloat = pedalAction.ReadValue<float>();
+
+        // get our right input up to 1, as long as it is more than our last rightInput
+        if (onRight)
+        {
+            // if our lastRight is less than 0.9
+            if (lastRight < 1)
+            {
+                if (pedalAction.ReadValue<float>() > lastRight)
+                {
+                    /// first, apply our force to the back wheel
+                    ApplyForce(pedalAction.ReadValue<float>() - lastRight);
+                    // then update the last movement amount
+                    lastRight = pedalAction.ReadValue<float>();
+                }
+            } else if (lastRight >= 1)
+            {
+                // if our lastRight is more than 0.9, we have bottomed out! We are no longer on our right foot
+                lastLeft = 0;
+                onRight = false;
+            }
+        }
+
+        // get our left input down to -1, as long as it is less than our last leftInput
+        if (!onRight)
+        {
+            // if our lastRight is less than 0.9
+            if (lastLeft > -1)
+            {
+                if (pedalAction.ReadValue<float>() < lastLeft)
+                {
+                    /// first, apply our force to the back wheel, in this case it has to be the absolute value because it is negative
+                    ApplyForce(Mathf.Abs(pedalAction.ReadValue<float>() - lastLeft));
+                    // then update the last movement amount
+                    lastLeft = pedalAction.ReadValue<float>();
+                }
+            }
+            else if (lastLeft <= -1)
+            {
+                // if our lastLeft is less than -0.9 then we have bottomed out, go to the right foot
+                lastRight = 0;
+                onRight = true;
+            }
+        }
+
+        // check our feet to update which foot we're on
+        if (rightFootPop.localPosition.y > 0.5)
+        {
+            lastRight = 0;
+            onRight = true;
+        }
+
+        if (leftFootPop.localPosition.y > 0.5)
+        {
+            lastLeft = 0;
+            onRight = false;
+        }
+
+    }
+
+    // we apply our force to the pedals and to the back wheel at the same time
+    void ApplyForce(float force)
+    {
+        // turn our pedals
+        pedals.AddTorque(new Vector3(force * pedalTorqueMultiplier, 0, 0), ForceMode.Impulse);
+
+        // apply that rotation to the back wheel
+        backWheel.AddTorque(new Vector3(force * backWheelTorqueMultiplier, 0, 0), ForceMode.Acceleration);
+    }
+
+    private void UpdateFeet()
+    {
+        rightFootPop.position = rightFootTarget.position;
+        leftFootPop.position = leftFootTarget.position;
+    }
 }
